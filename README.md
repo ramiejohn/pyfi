5a8d8fd49e5d6c1208a309dac1e859006b9c7b80
Dan <dan@Daniels-MacBook-Pro.local>
2013-07-01 22:16:15 -0500

    updated readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# PyFi... Finance in Python
+
+### What is PyFi???
+PyFi is my replacement for Quantmod in R. Quantmod is great, but I'm over using R. Besides who wouldn't want the greatness of GIL for financial analysis?
+
+Anyways, ultimately what I'm trying to accomplish with PyFi is get a framework setup where I can use machine learning techniques on technical indicators to build trading
+models based on classifications of the current data i.e. (buy, sell, hold, all on red).
+
+If you'd like to help and you promise not to screw up any calculations (well you can screw up and write bad code, just make sure it ends up in the data management parts) fork it!
+
+
+Things to do:
+
+- [ ] grab data with requests and get it into pandas
+- [ ] shameless make functions out of these: http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators
+- [ ] highjack code from R's quantmod
+- [ ] updated this list...
--- grabber.py
+++ grabber.py
@@ -1,7 +1,10 @@
 import requests
 import pandas as pd
 
+
 def get_tick(tick):
+    """ Come on bro... lets use some requests
+    """
     base_url = "http://ichart.finance.yahoo.com/table.csv?s="
     r = requests.get(base_url+tick)
     splits = r.text.split('\n')
@@ -9,15 +12,15 @@ def get_tick(tick):
     data = { i:[] for i in splits[0].split(',')}
     header = splits[0].split(',')
     for i in splits:
-        # gross.
+        # gross, gross, gross... somebody rewrite this!
         if 'Date' not in i:
             dlist = map(float,i.split(',')[1:len(i.split(','))])
             dlist.insert(0, i.split(',')[0])
             for i,j in enumerate(dlist):
                 data[header[i]].append(j)
-    # date gets DJ screwed up... filter it
+    # date gets DJ screwed up and ends up with a extra missing... filter it
     data['Date'] = filter(len,data['Date'])
     return pd.DataFrame(data)
 
-    return pd.DataFrame(data)
+
 
